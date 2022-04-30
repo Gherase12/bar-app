6298abdef2dc4ca73e2a0d4366bb4fd9bc0aed62
Gherase12 <c_gherase12@yahoo.ro>
2022-04-30 17:12:35 +0300

    error 5

--- sanity.js
+++ sanity.js
@@ -5,7 +5,7 @@ export const config = {
     projectId: process.env.NEXT_PUBLIC_SAINTY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SAINTY_DATASET || 'production',
   apiVersion: '2021-03-25',
-  userCdn: process.env.NODE_ENV === 'production',
+  userCdn: true,
 }
 
 export const sanityClient = createClient(config)
